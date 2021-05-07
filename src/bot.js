98b1f057b9f4fc5fed81c170907c5ee7dd2ef50f
Rob Sturcke <rob@rslabs.dev>
2021-05-06 23:34:18 -0400

    parse commands without whitespace starting with the prefix `!`

--- src/bot.js
+++ src/bot.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = '!';
 
 client.on('ready', () => {
-  console.log(
-    `The bot has logged in as username: ${client.user.username} - ${client.user.tag}`
-  );
+  console.log(`The bot has logged in as username: ${client.user.tag}`);
 });
 
 client.on('message', (message) => {
   if (message.author.bot) return;
-  if (message.content === 'hello') {
-    message.channel.send('hello');
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    console.log(CMD_NAME);
+    console.log(args);
   }
 });
 
