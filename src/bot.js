a16bad24d77efa9090c5469894906995c2d11d12
Rob Sturcke <rob@rslabs.dev>
2021-05-06 22:15:52 -0400

    if author return to prevent infinite loop
    if hello send hello back

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,16 @@ const { Client } = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
-  console.log('The bot has logged in as username: ', `${client.user.username}`);
+  console.log(
+    `The bot has logged in as username: ${client.user.username} - ${client.user.tag}`
+  );
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  if (message.content === 'hello') {
+    message.channel.send('hello');
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
