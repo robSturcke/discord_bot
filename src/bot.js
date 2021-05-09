c2e185d524c984d8eeccff0f03772ad8cda5bc73
Rob Sturcke <rob@rslabs.dev>
2021-05-08 21:06:44 -0400

    add WebhookClient for members to be able to announce

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 
-const { Client } = require('discord.js');
+const { Client, WebhookClient } = require('discord.js');
 const client = new Client({
   partials: ['MESSAGE', 'REACTION'],
 });
+
+const webhookClient = new WebhookClient(
+  process.env.WEBHOOK_ID,
+  process.env.WEBHOOK_TOKEN
+);
+
 const PREFIX = '!';
 
 client.on('ready', () => {
@@ -17,34 +23,37 @@ client.on('message', async (message) => {
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
-    if (!message.member.hasPermission('KICK_MEMBERS'))
-      return message.reply('You cannot kick with your current permissions.');
     if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You cannot kick with your current permissions.');
       if (args.length === 0) return message.reply('Please provide an ID');
       const member = message.guild.members.cache.get(args[0]);
-
       if (member) {
         member
           .kick()
           .then((member) => message.channel.send(`${member} was kicked.`))
           .catch((err) => message.channel.send('Permission is not granted.'));
       } else {
-        message.channel.send('That member is not in here.');
+        message.channel.send('That member was not found');
       }
-    }
-  } else if (CMD_NAME === 'ban') {
-    if (!message.member.hasPermission('BAN_MEMBERS'))
-      return message.reply('You cannot ban with your current permissions.');
-    if (args.length === 0) return message.reply('Please provide an ID');
-
-    try {
-      const user = await message.guid.members.ban(args[0]);
-      message.channel.send('User was banned successfully.');
-    } catch (err) {
-      console.log(err);
-      message.channel.send(
-        'An error occured. You may not have permissions, or user is not found.'
-      );
+    } else if (CMD_NAME === 'ban') {
+      if (!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply('You cannot ban with your current permissions.');
+      if (args.length === 0) return message.reply('Please provide an ID');
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send('User was banned successfully.');
+      } catch (err) {
+        console.log(err);
+        message.channel.send(
+          'An error occured. You may not have permissions, or user is not found.'
+        );
+      }
+    } else if (CMD_NAME === 'announce') {
+      console.log(args);
+      const msg = args.join(' ');
+      console.log(msg);
+      webhookClient.send(msg);
     }
   }
 });
